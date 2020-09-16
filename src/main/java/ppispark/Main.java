6e33508d398e455f93d2b30be28b7613f0f9b7d2
Francesco Palini <francesco.palini@uniroma1.it>
2020-09-16 18:06:36 +0200

    up Main

--- src/main/java/ppispark/Main.java
+++ src/main/java/ppispark/Main.java
@@ -2,14 +2,14 @@ package ppispark;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.*;
 import org.graphframes.GraphFrame;
-import ppispark.connectedComponents.Ncounter;
-import ppispark.connectedComponents.comparator;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,20 +24,20 @@ public class Main {
 
         if (local)
             spark = SparkSession
-                .builder()
-                .master("local[*]")
-                .appName("biograph")
-                .getOrCreate();
+                    .builder()
+                    .master("local[*]")
+                    .appName("biograph")
+                    .getOrCreate();
         else
             spark = SparkSession
-                .builder()
-                .master("yarn")
-                .appName("biograph")
-                .config("spark.executor.instances", "8")
-                .config("spark.executor.cores", "6")
-                .config("spark.executor.memory", "20g")
-                .config("spark.debug.maxToStringFields", "50")
-                .getOrCreate();
+                    .builder()
+                    .master("yarn")
+                    .appName("biograph")
+                    .config("spark.executor.instances", "8")
+                    .config("spark.executor.cores", "6")
+                    .config("spark.executor.memory", "20g")
+                    .config("spark.debug.maxToStringFields", "50")
+                    .getOrCreate();
 
         Dataset<Row> edges = spark.read().
                 option("header", "True").
@@ -54,35 +54,112 @@ public class Main {
 
         GraphFrame graph = GraphFrame.fromEdges(edges);
 
-        spark.sparkContext().setCheckpointDir("hdfs://master.local:8020/user/hduser/data/checkpoint");
-
-//        System.out.println("Number of vertices: " + graph.vertices().count());
-//        System.out.println("Number of edges: " + graph.edges().count());
-//        System.out.println("Number of computed degrees: " + graph.degrees().count());
-//        System.out.println("Number of computed triangleCounts: " + graph.triangleCount().run().count());
-//
-//        System.out.println("Number of BFS results from Q75760 to Q16531: " + graph.bfs().fromExpr("id = 'uniprotkb:Q75760'").toExpr("id = 'uniprotkb:Q16531'").run().count());
-//        System.out.println("Number of Connected Components :" + graph.connectedComponents().run().groupBy("component").count().count());
-//
-//        ArrayList<Object> landmarks = new ArrayList<>();
-//        landmarks.add("uniprotkb:B3KV54");
-//        landmarks.add("uniprotkb:Q16531");
-//
-//        graph.shortestPaths().landmarks(landmarks).run().show(5);
-//
-//        graph.bfs().fromExpr("id = 'uniprotkb:B3KV54'").toExpr("id = 'uniprotkb:Q16531'").run().show(5);
-//
-//        graph.connectedComponents().run().show(5);
+        spark.sparkContext().setCheckpointDir(local ? "checkpoint" : "hdfs://master.local:8020/user/hduser/data/checkpoint");
 
         Row[] rows = (Row[]) graph.vertices().head(100);
 
-        ArrayList<String> N = new ArrayList<>();
+        ArrayList<Object> N = new ArrayList<>();
 
         for (Row row : rows)
             N.add(row.getString(0));
 
-        System.out.println("Number of vertices of the graph: " + graph.vertices().count());
+        System.out.println("Number of vertices of the connected component: " + F1(graph, N, 4).vertices().count());
+        System.out.println("Connected component and relative number of nodes of N: " + F2(graph, N, 0));
     }
 
+    public static GraphFrame F1(GraphFrame graph, ArrayList<Object> N, int x) {
+        if (x > 0) {
+            Dataset<Row> paths = graph.shortestPaths().landmarks(N).run();
+            Dataset<Row> explodedPaths = paths
+                    .select(paths.col("id"), org.apache.spark.sql.functions.explode(paths.col("distances")))
+                    .filter("value<=" + x)
+                    .drop("key")
+                    .drop("value")
+                    .distinct();
+            Dataset<Row> edges = graph.edges().join(explodedPaths, graph.edges().col("src").equalTo(explodedPaths.col("id")));
+            edges = edges.withColumnRenamed("id", "id1");
+            edges = edges.join(explodedPaths, edges.col("dst").equalTo(explodedPaths.col("id")));
+            edges = edges.withColumnRenamed("id", "id2");
+            graph = GraphFrame.fromEdges(edges);
+        }
 
+        Dataset<Row> components = graph.connectedComponents().run();
+
+        Tuple2<Long, Integer> max = components.javaRDD()
+                .mapToPair(r -> new Tuple2<>(r.get(1), r.get(0)))
+                .mapToPair(new Ncounter(N))
+                .reduceByKey(Integer::sum)
+                .max(new NCountComparator());
+
+        return extractComponent(graph, components, max._1);
+    }
+
+    public static HashMap<Long, Integer> F2(GraphFrame graph, ArrayList<Object> N, int x) {
+
+        if (x > 0) {
+            Dataset<Row> paths = graph.shortestPaths().landmarks(N).run();
+            Dataset<Row> explodedPaths = paths
+                    .select(paths.col("id"), org.apache.spark.sql.functions.explode(paths.col("distances")))
+                    .filter("value<=" + x)
+                    .drop("key")
+                    .drop("value")
+                    .distinct();
+            Dataset<Row> edges = graph.edges().join(explodedPaths, graph.edges().col("src").equalTo(explodedPaths.col("id")));
+            edges = edges.withColumnRenamed("id", "id1");
+            edges = edges.join(explodedPaths, edges.col("dst").equalTo(explodedPaths.col("id")));
+            edges = edges.withColumnRenamed("id", "id2");
+            graph = GraphFrame.fromEdges(edges);
+        }
+
+        Dataset<Row> components = graph.connectedComponents().run();
+
+
+        JavaPairRDD<Long, Integer> intersections = components.javaRDD()
+                .mapToPair(r -> new Tuple2<>(r.get(1), r.get(0)))
+                .mapToPair(new Ncounter(N))
+                .reduceByKey(Integer::sum);
+
+        HashMap<Long, Integer> component_count = new HashMap<>();
+
+        for (Tuple2<Long, Integer> t : intersections.collect()) {
+            if (t._2 > 0)
+                component_count.put(t._1, t._2);
+        }
+
+        return component_count;
+    }
+
+    private static GraphFrame extractComponent(GraphFrame graph, Dataset<Row> components, long componentID) {
+        Dataset<Row> maxComponent = components.filter("component=" + componentID);
+        Dataset<Row> filteredEdges = graph.edges().join(maxComponent, graph.edges().col("src").equalTo(maxComponent.col("id")));
+
+        return GraphFrame.fromEdges(filteredEdges);
+    }
+
+    private static class Ncounter implements PairFunction<Tuple2<Object,Object>, Long, Integer> {
+        private final ArrayList<Object> N;
+
+        public Ncounter(ArrayList<Object> N) {
+            this.N=N;
+        }
+
+        @Override
+        public Tuple2<Long, Integer> call(Tuple2<Object, Object> t) {
+            return new Tuple2<>(Long.parseLong(t._1.toString()), N.contains(t._2)? 1 : 0);
+        }
+    }
+
+    private static class NCountComparator implements Comparator<Tuple2<Long,Integer>>, Serializable {
+
+        @Override
+        public int compare(Tuple2<Long, Integer> t1, Tuple2<Long, Integer> t2) {
+            if(t1._2 > t2._2) {
+                return 1;
+            }
+            else if (t1._2 < t2._2) {
+                return -1;
+            }
+            return 0;
+        }
+    }
 }
