cad67e2a96c169b549f6069a782eafedd555346e
Lorenzo <dirocco.1581944@studenti.uniroma1.it>
2020-09-23 13:20:17 +0200

    directory Scala

--- src/main/scala/operation.scala
+++
@@ -1,7 +0,0 @@
-object operation {
-  def addInt( a:Int, b:Int ) : Int = {
-    var sum:Int = 0
-    sum = a + b
-    return sum
-  }
-}
---
+++ src/main/scala/ppispark/operation.scala
@@ -0,0 +1,9 @@
+package ppispark
+
+object operation {
+  def addInt(a: Int, b: Int): Int = {
+    var sum: Int = 0
+    sum = a + b
+    return sum
+  }
+}
