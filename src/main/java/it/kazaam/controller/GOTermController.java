2b3cff39668867e214d1e3b6d279584f06f85ebd
Francesco <francesco.palini@uniroma1.it>
2020-11-04 15:11:03 +0100

    Controller uses GET

--- src/main/java/it/kazaam/controller/GOTermController.java
+++ src/main/java/it/kazaam/controller/GOTermController.java
@@ -4,10 +4,7 @@ package it.kazaam.controller;
 import it.kazaam.service.AnnotationService;
 import it.kazaam.service.GOTermService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import scala.Tuple3;
 
 import java.util.ArrayList;
@@ -25,13 +22,18 @@ public class GOTermController {
     /*
      *   CALCOLO DELLA SIMILARITA' TRA PROTEINE
      */
-    @PostMapping("/protein/similarity")
+    @GetMapping("/protein/similarity")
     public List<Tuple3<String, String, Double>> proteinSimilarity(@RequestParam List<String> proteins) {
         String protein1 = proteins.get(0);
         String protein2 = proteins.get(1);
+
         // Identifico i termini associati alle due proteine
         Set<Long> terms1 = annotationService.getDistinctGOTermByProtein(proteins.get(0));
         Set<Long> terms2 = annotationService.getDistinctGOTermByProtein(proteins.get(1));
+
+        System.out.println(terms1);
+        System.out.println(terms2);
+
         // Calcolo la similarità semantica di p1 con p2
         double p1 = goTermService.goTermSimilarity(terms1, terms2);
         double p2 = goTermService.goTermSimilarity(terms2, terms1);
